66ec2ecbafc26dd5bdbf9571f88151d5bac3ac21
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2025-12-09 11:39:22 +0530

    Initialize React Vite project with ESLint and Tailwind
    Set up a new React project using Vite, including configuration files for ESLint and Tailwind CSS. Added initial components (Navbar, News, Wrapper), assets, and base styles. Includes README and .gitignore for project documentation and housekeeping.

---
+++ src/components/News.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Wrapper from "./Wrapper";
+
+const News = () => {
+  return (
+    <>
+			<Wrapper>
+				
+			</Wrapper>
+    </>
+  );
+};
+
+export default News;
---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Wrapper = ({ childrean }) => {
+  return <div className="max-w-280 m-auto"> {childrean} </div>;
+};
+
+export default Wrapper;
